e7989e885a5585c46dad8c128b7710946a08da52
Jisu <51899708+jisu0309@users.noreply.github.com>
2025-06-29 13:23:56 +0900

    Time: 2 ms (98.49%), Space: 43.5 MB (57.97%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,28 +1,27 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
         int fr = 0;
         int bk = s.length()-1;
-        s=s.toLowerCase();
 
-        while(fr<bk){
-            if(!checkAlNu(s.charAt(fr))){
+        while(fr<=bk){
+            if(!checkAlpaNumeric(s.charAt(fr))){
                 fr++;
-            } else if(!checkAlNu(s.charAt(bk))){
+                continue;
+            } else if(!checkAlpaNumeric(s.charAt(bk))){
                 bk--;
-            } else if(s.charAt(fr)!=s.charAt(bk)){
+                continue;
+            } else if(s.charAt(fr++) != s.charAt(bk--)) {
                 return false;
-            } else {
-                fr++;
-                bk--;
             }
         }
 
         return true;
     }
 
-    public boolean checkAlNu(char c){
-        if(c>='a'&&c<='z') return true;
-        if(c>='0'&&c<='9') return true;
+    boolean checkAlpaNumeric(char c){
+        if(c>='a' && c<='z') return true;
+        if(c>='0' && c<='9') return true;
         return false;
     }
 }

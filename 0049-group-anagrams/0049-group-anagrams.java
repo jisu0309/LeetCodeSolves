c0bdc53d4d07a5eee71c87319b7af41e2234998a
Jisu <51899708+jisu0309@users.noreply.github.com>
2025-07-19 21:13:51 +0900

    Time: 4 ms (99.86%), Space: 46.7 MB (99.98%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,44 +1,20 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> ret = new ArrayList<>();
-        HashMap<String, Integer> map = new HashMap<>();
+    public List<List<String>> groupAnagrams(String[] strs) { 
+        HashMap<String, List<String>> map = new HashMap<>();
 
-        // for(String str:strs){
-        //     int[] arr = new int[26];
-        //     for(char c:str.toCharArray()){
-        //         arr[c-'a']++;
-        //     }
-        //     String arrstr = String.valueOf(arr);
-        //     System.out.printf("str=%s\n", arrstr);
-
-        //     Integer get = map.get(arrstr);
-        //     if(get==null) {
-        //         ret.add(new ArrayList<>(Arrays.asList(str)));
-        //         map.put(arrstr, ret.size()-1);
-        //     } else{
-        //         ret.get(get).add(str);
-        //     }
-
-        // }
-
-
-        for(String str:strs){
-            char[] carr = str.toCharArray();
-            Arrays.sort(carr);
-            String sorted = String.valueOf(carr);
-            Integer get = map.get(sorted);
-
-            // System.out.printf("str=%s, sorted=%s, get=%s\n", str, sorted, get);
-
-            if(get==null) {
-                ret.add(new ArrayList<>(Arrays.asList(str)));
-                map.put(sorted, ret.size()-1);
-            } else{
-                ret.get(get).add(str);
+        for(int i=0; i<strs.length; i++){
+            char[] carr = new char[26];
+            for(char c : strs[i].toCharArray()){
+                carr[c-'a']++;
             }
+            String st = new String(carr);
 
+            if(map.get(st)==null){
+                map.put(st, new ArrayList<>());
+            }
+            map.get(st).add(strs[i]);
         }
 
-        return ret;
+        return new ArrayList<>(map.values());
     }
 }

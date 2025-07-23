f9261efb713183018de2fd53036a1da3d24c3cc3
Jisu <51899708+jisu0309@users.noreply.github.com>
2025-07-23 23:09:06 +0900

    Time: 7 ms (34.46%), Space: 45 MB (23.71%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -6,24 +6,21 @@ class Solution {
         HashMap<Character, Integer> map = new HashMap<>();
 
         while(now<s.length()){
-            
-            // System.out.printf("now=%d%s, fr=%d%s, max=%d\n", now, s.charAt(now), fr, s.charAt(fr), max);
-            
             Integer last_position = map.get(s.charAt(now));
+            
             if(last_position==null){
                 map.put(s.charAt(now), now);
-                now++;
+                now++;                
             } else if(last_position<fr){
                 now++;
             } else{
                 max = Math.max(max, now-fr);
                 map.put(s.charAt(now), now);
-                fr = last_position + 1;
-                now++;
+                while(fr<=last_position){
+                    map.put(s.charAt(map.get(s.charAt(fr++))), null);
+                }
             }
         }
-
-        // System.out.printf("now=%d%s, fr=%d%s, max=%d\n", now, ' ', fr, s.charAt(fr), max);
 
         return Math.max(max, now-fr);
     }
